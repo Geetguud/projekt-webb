352667f9a5f46f2f500dec3af8bff326b42801ff
Geetguud <65584242+Geetguud@users.noreply.github.com>
2020-07-30 10:36:42 +0700

    javascript for simple-calculator

---
+++ simples/calculate.js
@@ -0,0 +1,106 @@
+"use strict"
+
+let currentValue = [""];
+
+function numEnter(num) {
+    if (!currentValue[currentValue.length - 1]) {
+        currentValue[currentValue.length - 1] = num;
+        display();
+    } else {
+        currentValue[currentValue.length - 1] = (currentValue[currentValue.length - 1] * 10) + num;
+        display();
+    }
+}
+
+function display() {
+    if (typeof currentValue[currentValue.length - 1] === "number") {
+        document.getElementById("result").innerHTML = currentValue.map(item => getNumDisplay(item)).join(" ");    
+    } else {
+        document.getElementById("result").innerHTML = currentValue.join(" ");
+    }
+}
+
+function getNumDisplay(num) {
+    if (typeof num === "string") {return num}
+    let resolve = num.toString().split("").reverse();
+    let result = [];
+    for (let i = 3; resolve.length; i--) {
+        if (i === 0) {
+            i += 3;
+            result.unshift(".");
+        }
+        result.unshift(resolve.shift());
+    }
+    return result.join("")
+}
+
+function operator(op) {
+    currentValue[currentValue.length - 1] = getNumDisplay(currentValue[currentValue.length - 1]);
+    if (!currentValue[currentValue.length - 1] && typeof currentValue[currentValue.length - 2] === "string") {
+        switch(op) {
+            case "+": currentValue[currentValue.length - 2] = "+"; break;
+            case "-": currentValue[currentValue.length - 2] = "-"; break;
+            case "x": currentValue[currentValue.length - 2] = "x"; break;
+            case "/": currentValue[currentValue.length - 2] = "/"; break;
+        }    
+    } else {
+        switch(op) {
+            case "+": currentValue.push("+"); break;
+            case "-": currentValue.push("-"); break;
+            case "x": currentValue.push("x"); break;
+            case "/": currentValue.push("/"); break;
+        }
+        currentValue.push("");
+    }
+    display();
+}
+
+function equals() {
+    if (!currentValue[currentValue.length - 1]) {currentValue.splice(-2, 2)}
+    if (currentValue.length === 1) {
+        display();
+        return
+    }
+    currentValue[currentValue.length - 1] = getNumDisplay(currentValue[currentValue.length - 1]);
+    let resolve = currentValue.filter((item, index) => !(index % 2)).map(item => Number(item.toString().split(".").join("")));
+    let operates = currentValue.filter((item, index) => index % 2);
+    if (operates.includes("/") || operates.includes("x")) {
+        for (let i = 0; i < operates.length; i++) {
+            if (operates[i] === 'x') {
+                let operate = resolve.splice(i, 2).reduce((total, item) => total * item);
+                resolve.splice(i, 0, operate);
+                operates.splice(i, 1);
+                i--;
+            }
+            if (operates[i] === '/') {
+                let operate = resolve.splice(i, 2).reduce((total, item) => total / item);
+                resolve.splice(i, 0, operate);
+                operates.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    for (let i = 0; i < operates.length; i++) {
+        if (operates[i] === '+') {
+            let operate = resolve.splice(i, 2).reduce((total, item) => total + item);
+            resolve.splice(i, 0, operate);
+            operates.splice(i, 1);
+            i--;
+        }
+        if (operates[i] === '-') {
+            let operate = resolve.splice(i, 2).reduce((total, item) => total - item);
+            resolve.splice(i, 0, operate);
+            operates.splice(i, 1);
+            i--;
+        }
+    }
+    if (!operates.length) {
+        currentValue = resolve.slice();
+        display();
+    } else {
+        console.log(currentValue);
+        console.log(resolve);
+        console.log(operates);
+        console.error("equals error");
+    }
+}
