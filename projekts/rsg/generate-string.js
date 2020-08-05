d186dd870c14b945b176e55679b4d8ae133f9805
Geetguud <65584242+Geetguud@users.noreply.github.com>
2020-08-05 12:26:58 +0700

    javascript random string generator

---
+++ projekts/rsg/generate-string.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let currentString = "";
+
+function generate() {
+    let result = [];
+    let length = document.getElementById("length").value;
+    let useAlphabets = document.getElementById("alphabets").checked;
+    let useNumbers = document.getElementById("numbers").checked;
+    let textCase = document.getElementById("dropdown").value;
+    if (!useAlphabets && !useNumbers) {return}
+    for (let i = 0; i < length; i++) {
+        result.push(getRandom(useAlphabets, useNumbers, textCase));
+    }
+    if (result === currentString) {generate(); return}
+    currentString = result.join("");
+    document.getElementById("display").innerHTML = result.join("");
+}
+
+function getRandom(useAlphabets, useNumbers, textCase) {
+    let cond, min, max;
+    if (useAlphabets && useNumbers) {
+        cond = Math.floor(Math.random() * 100) + 1;
+        if (cond <= 50) {
+            if (textCase === "mixed") {
+                let cond2 = Math.floor(Math.random() * 100) + 1;
+                if (cond2 <= 50) {return getRandomChar(97, 122)}
+                if (cond2 > 50) {return getRandomChar(65, 90)}
+            }
+            if (textCase === "lower") {return getRandomChar(97, 122)}
+            if (textCase === "upper") {return getRandomChar(65, 90)}
+        } else {
+            return getRandomChar(48, 57)
+        }
+    }
+}
+
+function getRandomChar(min, max) {
+    let charCode = Math.floor(Math.random() * (max - min + 1)) + min;
+    return String.fromCharCode(charCode)
+}
