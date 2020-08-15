59fbed3f68943f1632d0a74bfc1c37d7c0cf1863
Geetguud <maman021129@gmail.com>
2020-08-15 21:37:34 +0700

    Redesign & Add new quotes

--- projekts/quote-gen/quotes.js
+++ projekts/quote-gen/quotes.js
@@ -18,5 +18,15 @@ let quotes =  [
     "The quality of a society can more important than your place in that society.",
     "Everything we do in life is a trade-off. Choosing to do one thing means not doing something else. The price of anything is the amount of life you exchange for it.",
     "We always think we need more. More help, more motivation, more energy. But in our current world the answer is often the exact opposite. We need less, fewer distractions, fewer goals, fewer responsibilities.",
-    "Fantasizing gives us the reward before we have accomplished the task and saps the energy we need to realize it."
+    "Fantasizing gives us the reward before we have accomplished the task and saps the energy we need to realize it.",
+    "God isn't finished with you just yet.",
+    "One who thinks and reflects develops his foresight and vision.",
+    "The true greatness of man lies in his capacity for eternal progress.",
+    "Knowledge is not what is memorised. Knowledge is what benefits.",
+    "Never have I dealt with anything more difficult than my own soul, which sometimes helps me and sometimes opposes me.",
+    "Peace and Jealousy can not live in the same heart... one of them must leave.",
+    "When the heart weeps for what it has lost, the spirit laughs for what it has found.",
+    "You won't understand why it's so hard to change people until you realize how hard it is to change yourself.",
+    "When you stop chasing the wrong things, you give the right things a chance to catch you.",
+    "And in the end, it's not the years in your life that count. It's the life in your years."
 ]
